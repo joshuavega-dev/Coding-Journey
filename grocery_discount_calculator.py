00cfe823f5495330600c0435d89c0c9c854adf5b
joshuavega-dev <joshuavegadev@gmail.com>
2026-03-21 18:09:48 +0800

    Create grocery_discount_calculator.py
    This is my first Project as an IT Student

---
+++ grocery_discount_calculator.py
@@ -0,0 +1,23 @@
+customer_name = input ("Enter Your Name: ")
+item = input ("Enter Item Name: ")
+cost = float (input ("Enter Item Cost: "))
+quantity = int(input("Enter Item Quantity: "))
+total = cost * quantity 
+
+#discount student,pwd,senior
+if total > 100:
+    discount = total * .10
+    total = total - discount
+    print ("Congrats you received a 10% discount!")
+
+elif total > 60:
+    discount = total * .6
+    total = total - discount 
+    print ("Congrats you received a 6% discount!")
+
+else:
+    print ("Thank you for the purchase!")   
+
+print (f"You successfully bought {quantity} x {item}!")
+print (f"total bill {total:.2f}$")
+print ("Thank you. so much for the purchase!")
