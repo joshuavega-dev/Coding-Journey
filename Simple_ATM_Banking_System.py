108b1bb6c071db02d79c307a487c8d0bba5ffafe
joshuavega-dev <joshuavegadev@gmail.com>
2026-03-21 20:54:55 +0800

    Create Simple_ATM_Banking_System.py

---
+++ Simple_ATM_Banking_System.py
@@ -0,0 +1,32 @@
+#ATM BANK SYSTEM
+balance = 1000.00
+
+print("\n ATM BANK SYSTEM")
+while True: 
+    print("\nATM BANK SYSTEM")
+    print("1. Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+
+    menu = input("\nSelect an option: ")
+
+    if menu == "1":
+        print(f"Current Balance: ₱{balance:,.2f}")
+
+    elif menu == "2":
+        amount = float(input("Enter Deposit Amount: "))
+        balance += amount
+        print(f"Successfully Deposit ₱{amount:,.2f}")
+
+    elif menu == "3":
+        amount = float(input("Enter Withdraw Amount: "))
+        if amount > balance:
+            print("Insufficient funds!")
+        else:
+            balance -= amount
+            print(f"Successfully withdrawn ₱{amount:,.2f}")
+
+    elif menu == "4":
+        print("Thank you for Banking with us. Have a great day!")
+        break
