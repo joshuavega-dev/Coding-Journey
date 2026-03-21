2c9c1f7ece10cb4185bd154fe1663bdf459babc9
joshuavega-dev <joshuavegadev@gmail.com>
2026-03-21 19:38:13 +0800

    Update Simple_Calculator.py
    clean and functional Python script that performs basic mathematical operations based on user input.

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -6,19 +6,22 @@ num2 = float (input ("Enter 2nd Number: "))
 
 if operator == "+":
     result = num1 + num2
-    print(result)
+    print(round(result, 3))
 
 elif operator == "-":
     result = num1 - num2
-    print(result)
+    print(round(result, 3))
 
 elif operator == "*":
     result = num1 * num2
-    print(result)
+    print(round(result, 3))
 
 elif operator == "/":
     if num2 != 0:
         result = num1 / num2
-        print(result)
+        print(round(result, 3))
     else:
         print("Error: Cannot divide by zero!")
+
+else:
+    print (f"{operator} is invalid! Please use (+, -, *, /) as an operator")
